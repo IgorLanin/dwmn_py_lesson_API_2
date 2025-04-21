31fb31f0731a76eff287b309d2206f1ceaaf6796
Igor <lanin.igor@gmail.com>
2025-04-21 03:56:23 +0200

    Оптимизировал работу скрипта.

--- main_v2.py
+++ main_v2.py
@@ -5,7 +5,7 @@ from urllib.parse import urlparse
 
 
 def shorten_link(token, link):
-    url_api_shorten = "https://api.vk.ru/method/utils.getShortLink"
+    shorten_api_url = "https://api.vk.ru/method/utils.getShortLink"
 
     payload = {
         "access_token": token,
@@ -14,27 +14,29 @@ def shorten_link(token, link):
         "v": "5.199"
     }
 
-    response = requests.post(url_api_shorten, params=payload)
+    response = requests.post(shorten_api_url, params=payload)
     response.raise_for_status()
 
-    return response.json()
+    short_link = response.json()['response']['short_url']
+
+    return short_link
 
 
 def count_clicks(token, link):
-    url_api_clicks = "https://api.vk.ru/method/utils.getLinkStats"
+    count_clicks_api_url = "https://api.vk.ru/method/utils.getLinkStats"
 
     parsed_url = urlparse(link)
 
-    key_url = parsed_url[2].replace("/", "")
+    short_url_key = parsed_url[2].replace("/", "")
 
     payload = {
         "access_token": token,
-        "key": key_url,
+        "key": short_url_key,
         "interval": "forever",
         "v": "5.199"
     }
 
-    response = requests.post(url_api_clicks, params=payload)
+    response = requests.post(count_clicks_api_url, params=payload)
     response.raise_for_status()
 
     clicks = response.json()['response']['stats'][0]['views']
@@ -42,35 +44,65 @@ def count_clicks(token, link):
     return clicks
 
 
-def is_shorten_link(url):
-    parsed = urlparse(url)
-    for i in parsed:
-        if i == "vk.cc":
-            return True
+def is_shorten_link(token, url):
+    check_link_api_url = "https://api.vk.ru/method/utils.checkLink"
+
+    payload = {
+        "access_token": token,
+        "url": url,
+        "v": "5.199"
+    }
+
+    response = requests.post(check_link_api_url, params=payload)
+    response.raise_for_status()
+
+    response_data = response.json()['response']['link']
+
+    if url != response_data:
+        return True
     return False
+
+
+def get_api_response(token, url):
+    check_link_api_url = "https://api.vk.ru/method/utils.checkLink"
+
+    payload = {
+        "access_token": token,
+        "url": url,
+        "v": "5.199"
+    }
+
+    response = requests.post(check_link_api_url, params=payload)
+    response.raise_for_status()
+
+    return response.json()
 
 
 def main():
     load_dotenv()
 
-    token = os.getenv("VK_ID_TOKEN")
+    token = os.getenv("VK_ID_TOKEN", default=None)
+
+    if token is None or not token:
+        return print("В программе отсутствует токен.Обратитесь к администратору.")
+
     user_input = input("Введите ссылку для сокращения: ")
 
-    if is_shorten_link(user_input) is True:
-        try:
+    try:
+        answer = is_shorten_link(token, user_input)
+        if answer is True:
             clicks = count_clicks(token, user_input)
             print("Количество переходов по ссылке: ", clicks)
-        except:
-            print("Что-то пошло не так. Попробуйте повторить запрос позже.")
-    else:
-        try:
-            short_link = shorten_link(token, user_input)['response']['short_url']
+        else:
+            short_link = shorten_link(token, user_input)
             print("Сокращенная ссылка: ", short_link)
-        except:
-            if shorten_link(token, user_input)['error']['error_code'] == 5:
-                print("Ошибка авторизации. Проверьте правильность токена")
-            elif shorten_link(token, user_input)['error']['error_code'] == 100:
-                print("Вы ввели неверную ссылку. Ошибка: ", shorten_link(token, user_input)['error']['error_msg'])
+
+    except KeyError:
+        response_data = get_api_response(token, user_input)
+        if response_data['error']['error_code'] == 5:
+            print("Ошибка авторизации. Проверьте правильность токена.")
+        elif response_data['error']['error_code'] == 100:
+            print("Вы ввели неверную ссылку. Ошибка: ", response_data['error']['error_msg'])
 
 
 if __name__ == '__main__':
